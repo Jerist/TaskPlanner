1775301f5d6394344752eb46b565e7cb20eed6bc
Bulavin Gleb <gleb2003bulav@gmail.com>
2024-04-21 21:41:16 +0300

    add entity classes

---
+++ src/main/java/entity/Priority.java
@@ -0,0 +1,5 @@
+package entity;
+
+public enum Priority {
+    Low, Average, High
+}
---
+++ src/main/java/entity/TaskInProject.java
@@ -0,0 +1,21 @@
+package entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode
+@Builder
+public class TaskInProject {
+    private Long idTaskInProject;
+    private Task task;
+    private Project project;
+    private LocalDateTime dateOfAddition;
+}
